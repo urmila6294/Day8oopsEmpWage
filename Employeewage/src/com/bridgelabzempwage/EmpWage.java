32e32dbfdee60030f5cdf160bc55f9891755c237
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-13 00:04:55 +0530

    Added computed emp wage problem using class and variables

--- Employeewage/src/com/bridgelabzempwage/EmpWage.java
+++ Employeewage/src/com/bridgelabzempwage/EmpWage.java
@@ -5,30 +5,39 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NO_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
+	public static final int NUM_OF_WORKING_DAYS = 30;
 	
-	public static void main(String[] args) { 
-		int empHrs = 0 , totalEmpHrs = 0, totalWorkingDays = 0;
-		int totalEmpWage = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS){
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random()*10)%3;
-			switch(empCheck) {
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				default:
-					empHrs = 0;
-			}
-			totalEmpHrs += empHrs;
-			System.out.println("Day:" +totalWorkingDays  +  "EmpHrs:" +empHrs);
-		}
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("total Emp Wage:" +totalEmpWage);
+	public static int computeEmpWage() {
+		// variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+				totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 4;
+				break;
+			case IS_PART_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+				
+			}
+			
+			totalEmpHrs += empHrs;
+			System.out.println("Days:" +totalWorkingDays+ "EmpHrs:" +empHrs);
+		} 
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage:" +totalEmpWage);
+		return totalEmpWage;
 	}
+	
+	public static void main(String[] args) {
+		computeEmpWage();
+
 	}
+}
