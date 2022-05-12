17bde97c6863ebd0a23ccab5acdc3d2023b82749
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-12 23:51:18 +0530

    Added total monthly wage problem

--- Employeewage/src/com/bridgelabzempwage/EmpWage.java
+++ Employeewage/src/com/bridgelabzempwage/EmpWage.java
@@ -5,27 +5,30 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage computation program");
-
+	public static final int NO_OF_WORKING_DAYS = 20;
+	
+	public static void main(String[] args) { 
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) Math.floor(Math.random()*10)%3;
-		
-		switch(empCheck) {
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
-				
+		int totalEmpWage = 0;
+		for(int day = 0;day < NO_OF_WORKING_DAYS;day++) {
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+			switch(empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			empWage = empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage:" + empWage);
 		}
+		System.out.println("total Emp Wage:" +totalEmpWage);
 		
-		empWage = empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:" + empWage);
-}
+	}
+
 }
