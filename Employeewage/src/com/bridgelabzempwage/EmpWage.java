d0c598e969506d84884f5ba66d1cac46b4019993
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-12 23:15:51 +0530

    Added employee daily wage program

--- Employeewage/src/com/bridgelabzempwage/EmpWage.java
+++ Employeewage/src/com/bridgelabzempwage/EmpWage.java
@@ -4,14 +4,25 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
-		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-		}
-		else {
-			System.out.println("Employee is Absent");
+		 	int IS_FULL_TIME = 1;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
+			int empWage = 0;
+			double empCheck = Math.floor(Math.random()*10)%2;
+			
+			if (empCheck == IS_FULL_TIME) {
+				System.out.println("Employee is Present");
+				empHrs = 8;
+			}
+				
+			else {
+				System.out.println("Employee is Absent");
+				empHrs = 0;
+			}
+			
+			empWage = empHrs*EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage:" + empWage);
 		}
 	}
 
-}
+
