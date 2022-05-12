17bdd0f4574ec04b3f7a17b1a12a0db6db77c3e5
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-12 23:07:36 +0530

    Added employee attendance program

--- Employeewage/src/com/bridgelabzempwage/EmpWage.java
+++ Employeewage/src/com/bridgelabzempwage/EmpWage.java
@@ -4,6 +4,14 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
